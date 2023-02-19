9aa764773c98bb852b04382ad7c2d409b7a1e910
ahmethakann <99harkan@gmail.com>
2023-02-19 16:17:35 +0300

    base version of project

--- ozguven_website/ozguven_website/settings.py
+++ ozguven_website/ozguven_website/settings.py
@@ -18,9 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = int(os.environ.get('DEBUG', default=0))
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
+SECRET_KEY = "django-insecure-v8bzoj5)*&_%x-yy7o*z-2$*m1uuo*hbtb(n)%@bboej@%wkox"
+DEBUG = int(os.environ.get('DEBUG', default=1))
+ALLOWED_HOSTS = []
+#os.environ.get('ALLOWED_HOSTS').split(",")
 
 
 # Application definition
