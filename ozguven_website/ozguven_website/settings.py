3eec29603d60adddb84a1d7d8fead6ddafb07161
Ahmet Hakan Özgüven <99harkan@gmail.com>
2023-03-23 06:35:24 +0300

    asfa

--- ozguven_website/ozguven_website/settings.py
+++ ozguven_website/ozguven_website/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'ozguven_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
